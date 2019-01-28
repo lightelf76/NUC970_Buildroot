78139a9c03a6c95446807d143e0ddcee0c76216c
lightelf76 <valerygaynullin@yandex.ru>
2019-01-28 19:13:36 +0300

    Add demos to build

--- board/nuvoton/install_rootfs_disp976.sh
+++ board/nuvoton/install_rootfs_disp976.sh
@@ -1,3 +1,8 @@
 #!/bin/sh
+APP_PATH=output/build/applications-1.0.0
+
 rm output/target/etc/resolv.conf
 cp -a board/nuvoton/rootfs-disp976/* output/target/
+if [ -d $APP_PATH ]; then
+	cp $APP_PATH/demos/* output/target/usr/bin/
+fi
